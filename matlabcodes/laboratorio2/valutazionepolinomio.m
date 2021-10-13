05064821a52644cd9cf90170d3beea8e97cd3909
Cirdans-Home <f.durastante@na.iac.cnr.it>
2021-10-13 15:24:17 +0200

    enriched example

--- matlabcodes/laboratorio2/valutazionepolinomio.m
+++ matlabcodes/laboratorio2/valutazionepolinomio.m
@@ -1,13 +1,19 @@
 %% Valutazione del polinomio con Roundoff
 
 x = 0.988:.0001:1.012;
-y = x.^7 - 7*x.^6 + 21*x.^5 - 35*x.^4 + 35*x.^3 -21*x.^2 + 7*x - 1 ;
+y1 = x.^7 - 7*x.^6 + 21*x.^5 - 35*x.^4 + 35*x.^3 -21*x.^2 + 7*x - 1 ;
 figure(1)
 subplot(1,2,1)
-plot(x,y)
+plot(x,y1)
 
 %% Roundoff
-y = (x-1).^7;
+y2 = (x-1).^7;
 figure(1)
 subplot(1,2,2)
-plot(x,y)
+plot(x,y2)
+
+%% Paragone
+figure(2)
+plot(x,y1,'-',x,y2,'--','LineWidth',2);
+legend({'Polinomio Esteso','Polinomio Raccolto'},...
+    'Location','best','FontSize',18);
