b51dab89facb350ab7808e25a78f1fc5f624a218
Cirdans-Home <f.durastante@na.iac.cnr.it>
2021-12-11 19:30:42 +0100

    Aggiunta versione senza ciclo for

--- matlabcodes/laboratorio8/trapezir.m
+++ matlabcodes/laboratorio8/trapezir.m
@@ -13,13 +13,18 @@ else
     n = 2^(k -2);  % Numero dei nuovi punti:
     h = (b - a)/n ; % Spaziatura
     x = a + h/2.0;  % Coordinata del primo dei nuovi punti
-    sum = 0.0;
-    for i = 1:n
-        fx = f(x);
-        sum = sum + fx;
-        x = x + h;
-    end
-    Ih = (I2h + h*sum)/2.0;
+%     sum = 0.0;
+%     for i = 1:n
+%         fx = f(x);
+%         sum = sum + fx;
+%         x = x + h;
+%     end
+    % Versione senza ciclo for:
+    x = x + (0:(n-1))*h;
+    fx = f(x);
+    Inew = sum(fx);
+    Ih = (I2h + h*Inew)/2.0;
+    % Ih = (I2h + h*sum)/2.0; % Version con ciclo for
 end
 
 end
