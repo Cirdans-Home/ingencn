307842e2836cace1e3b664fdccda04fa9d2e0b04
Cecilia <ce.pagliantini@gmail.com>
2023-05-03 14:08:40 +0200

    aggiornata function per interpolazione di Lagrange

--- matlabcodes/laboratorio9/LagrangePoly.m
+++ matlabcodes/laboratorio9/LagrangePoly.m
@@ -1,5 +1,5 @@
 function L = LagrangePoly(x, z, j)
-%%LAGRANGEPOLY costruisce il k-esimo polinomio di Lagrange dati i nodi
+%%LAGRANGEPOLY costruisce il j-esimo polinomio di Lagrange dati i nodi
 % di interpolazione x e lo valuta nei vettore z
 %    INPUT:
 %          x vettore riga degli n+1 nodi di interpolazione
@@ -12,5 +12,12 @@ n = length(x);
 assert(j<=n);
 idx = [1:j-1,j+1:n];
 L = prod(repmat(z,n-1,1)-repmat(x(idx)',1,length(z)),1)/prod(x(j)-x(idx));
+
+% Alternativa con loop for
+% L = zeros(length(z),1);
+% for k = 1:length(z)
+%     L(k) = prod((z(k)-x(idx))./(x(j)-x(idx)));
+% end
+
 
 end
